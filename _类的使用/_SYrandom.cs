cdf94fc65cf7595688df4d304e21fc9223283457
L0 <363222206@qq.com>
2023-12-18 02:31:48 +0800

    添加项目文件。

---
+++ _类的使用/_SYrandom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _类的使用
+{
+    internal class _SYrandom
+    {
+        
+        public class Number1
+        {
+            Random random = new Random();
+          
+    
+       
+        }
+    }
+}
